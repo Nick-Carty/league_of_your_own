102184c3c2f5df65d59a4b0b6688ea54dce5cb7c
Nicholas Carty <nickcarty13@gmail.com>
2018-11-26 14:47:59 -0500

    Added League table, model, controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_184829) do
+ActiveRecord::Schema.define(version: 2018_11_26_192136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "leagues", force: :cascade do |t|
+    t.string "name", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
